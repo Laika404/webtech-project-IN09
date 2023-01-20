fd79b77e5a7f2031b843c960e6677b0f054892d9
Sietse van de Griend <sietsevdgriend@gmail.com>
2023-01-20 12:05:46 +0100

    nav bar fully developed + php versions

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 An uva webtechnology project describing a website.
 
 Made by:
+Kein Jansen: 14130548.
+Jaafar Kadim: 14435285.
+Francesco Pavlović: 13782118.
+Björn Smitt: 14663600.
 Sietse van de griend : 12652776
-....
---
+++ nav-bar-head.php
@@ -0,0 +1,8 @@
+<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+        <link rel="icon" href="gea-icon.png" sizes="64x64">
+        <link rel="stylesheet" href="nav-bar-style.css">
+        <!--Icon libraries -->
+        <script type="module" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule="" src="https://unpkg.com/ionicons@5.0.0/dist/ionicons/ionicons.js"></script>
+        <script src="https://kit.fontawesome.com/f0df45953b.js" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
